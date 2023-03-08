d776af8fbbc59fddace5370e7c1109d5fad6e5e8
annadysinger <97917461+annadysinger@users.noreply.github.com>
2023-03-08 17:26:51 -0500

    Added to main

--- SplayTree.h
+++ SplayTree.h
@@ -157,18 +157,18 @@ private:
     }
 
     // Helper recursive function to find a value in the tree.
-    bool find(const Comparable& c, SplayNode* &n) {
+    bool find(const Comparable& c, SplayNode* &n, int &depth) {
         if (n == nullptr) {
             // Reached a dead end. Value not in tree.
             return false;
         }
         if (c < n->value) {
             // Value is less than current node. Go to node's left child.
-            return find(c, n->leftChild);
+            return find(c, n->leftChild, depth++);
         }
         if (n->value < c) {
             // Value is greater than current node. Go to node's right child.
-            return find(c, n->rightChild);
+            return find(c, n->rightChild, depth++);
         }
         // If code reaches here, c == n->value. Node found!
         splay(n);
@@ -265,7 +265,8 @@ public:
 
     bool find(const Comparable& c) {
         // calls private helper function
-        return find(c, root);
+        int depth = 0;
+        return find(c, root, depth);
     }
 
     void add(const Comparable& c) {
--- main.cpp
+++ main.cpp
@@ -28,61 +28,71 @@ int main() {
         std::cout << "Something went wrong." << std::endl;
     }
 
-    // Instantiate a binary search tree (of integer values) and insert the
-    // numbers 1 through 100 in that order, then search for all numbers 1 through 100.
-    BinarySearchTree<int> bstOne;
-    for(int i = 1; i <= 100; i++){
-        bstOne.add(i);
-    }
-    // Print in order
-    std::cout << "Inserted in order\n" << std::endl;
-    std::cout << std::string(40, '-') << std::endl;
-    int depth;
-    bool result;
-    for (int i = 1; i <= 100; i++){
-        result = bstOne.find(i, depth);
-        std::cout << "Searching for " << i << ", result: " << result << ", depth: " << depth << std::endl;
-    }
-    // testing
-    result = bstOne.find( 0, depth);
-    std::cout << "Searching for " << 0 << ", result: " << result << ", depth: " << depth << std::endl;
-    std::cout << std::string(40, '-') << std::endl;
-
-    // Now instantiate a new BST and insert the numbers 1 through 100 in random order.
-    // You may use https://www.random.org to generate a shuffled list of numbers 1 through 100,
-    // or you can shuffle a vector of numbers 1 through 100 using C++’s shuffle method.
-    std::random_device rd;
-    std::vector<int> vec;
-    for(int i = 1; i <= 100; i++){
-        vec.push_back(i);
-    }
-    // Requires std::random_device rd; from above ^^^
-    std::shuffle(vec.begin(), vec.end(), rd);
-
-    // Prove that it's shuffled
-    for (int& n : vec) {
-        std::cout << n << " ";
-    }
-    std::cout << std::endl;
-
-    // Now search for each number from 1 through 100,
-    // and record the depth of each find operation to a file.
-    BinarySearchTree<int> bstTwo;
-    for(int i = 0; i <= vec.size(); i++){
-        bstTwo.add(vec[i]);
-    }
-    std::cout << "Inserted in Random Order\n" << std::endl;
-    std::cout << std::string(40, '-') << std::endl;
-    for (int i = 1; i <= 100; i++){
-        result = bstTwo.find(i, depth);
-        std::cout << "Searching for " << i << ", result: " << result << ", depth: " << depth << std::endl;
-    }
 
     // Now instantiate a BST to hold objects of your custom data type.
     // Read your objects into a vector (as you have done in earlier projects) and then insert them into the BST.
     // Make sure you have at least 1000 objects in your BST.
     // Now search for each of your objects and record its depth in a file.
+    BinarySearchTree<int> bst1;
+    BinarySearchTree<int> bstTwo;
+    std::ofstream outFile("../data/BSTdata.txt");
+    outFile.open("../data/BSTdata.txt");
+    if (outFile.is_open()) {
+        // Instantiate a binary search tree (of integer values) and insert the
+        // numbers 1 through 100 in that order, then search for all numbers 1 through 100.
+        for (int i = 1; i <= 100; i++) {
+            bst1.add(i);
+        }
+        // Print out in order
+        outFile << "Inserted in order\n" << std::endl;
+        outFile << std::string(40, '-') << std::endl;
+        int depth = 0;
+        bool result;
+        for (int i = 1; i <= 100; i++){
+            result = bst1.find(i, depth);
+            outFile << "Searching for " << i << ", result: " << result << ", depth: " << depth << std::endl;
+        }
+        // Print shuffled
+        // Now instantiate a new BST and insert the numbers 1 through 100 in random order.
+        std::random_device rd;
+        std::vector<int> vec;
+        for(int i = 1; i <= 100; i++){
+            vec.push_back(i);
+        }
+        // Requires std::random_device rd; from above ^^^
+        std::shuffle(vec.begin(), vec.end(), rd);
 
+        // Prove that it's shuffled
+        for (int& n : vec) {
+            std::cout << n << " ";
+        }
+        std::cout << std::endl;
 
+        // Now search for each number from 1 through 100,
+        // and record the depth of each find operation to a file.
+        BinarySearchTree<int> bstTwo;
+        for(int i = 0; i <= vec.size(); i++){
+            bstTwo.add(vec[i]);
+        }
+        outFile << "Inserted in Random Order\n" << std::endl;
+        outFile << std::string(40, '-') << std::endl;
+        for (int i = 1; i <= 100; i++){
+            result = bstTwo.find(i, depth);
+            outFile << "Searching for " << i << ", result: " << result << ", depth: " << depth << std::endl;
+        }
+        // Searching for 0
+        bst1.find(0, depth);
+        outFile << "depth: " << depth << std::endl;
+        depth = 0;
+        bst1.find(101, depth);
+        outFile << "depth: " << depth << std::endl;
+
+        bst1.timber();
+        bstTwo.timber();
+        outFile.close();
+    } else {
+        outFile << "Unable to open output file!" << std::endl;
+        return 1;
+    }
     return 0;
 }
